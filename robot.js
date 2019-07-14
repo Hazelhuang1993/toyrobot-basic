1011aec92b924794a495c2d0f002479bb1996a40
Hazel <huckleberryww@hotmail.com>
2019-07-14 19:43:45 +1000

    note
    note

--- robot.js
+++ robot.js
@@ -1,3 +1,4 @@
+
 class Game {
     constructor(mapSize, initPosition, icon) {
         this.state = {
@@ -17,15 +18,15 @@ class Game {
     
     move(newPosition) {
         if (this.availablePosition(newPosition, this.state.mapSize)) {
-            this.state.robotPosition = newPosition;
+            this.state.robotPosition = initPosition;
             this.render();
             return true;
         } else {
             return false;
         }
     }
-    
-    render() {
+                  // 1. var that = this;
+    render() {   // 2. 直接改成=> es6 arrow function 箭头函数可以修复this指针的问题
         var mapCells = document.querySelectorAll('.map-cell');
         mapCells.forEach(function(aCell, index) {
             if (index === this.state.robotPosition) {
@@ -43,4 +44,4 @@ class Game {
 }
 
 var game = new Game(5, 2, 'R');
-game.render();
+game.render();  
